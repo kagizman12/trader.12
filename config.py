261771e869385f66c56c9ac7fa758f01aca34a4d
kagizman12 <furkancoram@outlook.com>
2025-04-13 15:40:38 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+# config.py
+
+# Takip edilecek hisse senetleri
+SYMBOLS = ["THYAO", "SISE", "ASELS", "KRDMD", "TUPRS"]
+
+# Hacim patlama eşiği (örnek: %50 fazlası)
+VOLUME_SPIKE_MULTIPLIER = 1.5
+
+# Ortalama alınacak gün sayısı (TradingView üzerinden çekilen MA5 kullanılıyor)
+AVG_VOLUME_PERIOD = 5
